e9d229fad2b251fe7df114255a69ca75032b9b00
mglrahul <mglrahul@gmail.com>
2017-02-11 16:32:02 +0530

    delete the es6 code

--- server_with_es6_syntax/models/category.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-
-let catSchema = new Schema({
-    name: { type: String},
-    slug: {type: String}
-})
-
-module.exports = mongoose.model('Category', catSchema);
